0b62a30aea11d28569c7b96e7cf2e1d81d34bd61
Void <80608102+xptea@users.noreply.github.com>
2025-07-23 22:13:31 -0400

    past text

--- src-tauri/src/textinjection.rs
+++ src-tauri/src/textinjection.rs
@@ -1,4 +1,5 @@
 use enigo::{Enigo, KeyboardControllable};
+use arboard::Clipboard;
 
 pub fn inject_text(text: &str, restore_focus: Option<Box<dyn FnOnce()>>) {
     if let Some(restore_fn) = restore_focus {
@@ -6,5 +7,21 @@ pub fn inject_text(text: &str, restore_focus: Option<Box<dyn FnOnce()>>) {
     }
     
     let mut enigo = Enigo::new();
-    enigo.key_sequence(text);
+    let mut clipboard = Clipboard::new().unwrap();
+    
+    let _ = clipboard.set_text(text);
+    
+    #[cfg(target_os = "macos")]
+    {
+        enigo.key_down(enigo::Key::Meta);
+        enigo.key_click(enigo::Key::Layout('v'));
+        enigo.key_up(enigo::Key::Meta);
+    }
+    
+    #[cfg(not(target_os = "macos"))]
+    {
+        enigo.key_down(enigo::Key::Control);
+        enigo.key_click(enigo::Key::Layout('v'));
+        enigo.key_up(enigo::Key::Control);
+    }
 } 
